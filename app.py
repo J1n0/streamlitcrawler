6ad728154af58f204fd918bd7b307fd42b8a9708
J1n0 <117600126+J1n0@users.noreply.github.com>
2025-06-25 06:00:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ from google_play_scraper import app, reviews, search
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 import matplotlib.pyplot as plt
-plt.rcParams['font.family'] ='Malgun Gothic'
-plt.rcParams['axes.unicode_minus'] =False
-
+plt.rc('font', family='NanumGothic')
 
 # tokenizer 정의 (직렬화된 모델에서 필요)
 def simple_tokenizer(text):
